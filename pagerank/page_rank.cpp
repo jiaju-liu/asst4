8422f0971a9fbcc450679f4bc929c84ccc34e2fc
Jiaju Liu <jiajuliu@stanford.edu>
2021-11-25 10:49:58 -0800

    pagerank passes all tests

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -1,6 +1,6 @@
 #include "page_rank.h"
 
-#include <stdlib.h>
+#include <vector>
 #include <cmath>
 #include <omp.h>
 #include <utility>
@@ -23,77 +23,74 @@ using namespace std;
 void pageRank(Graph g, double* solution, double damping, double convergence)
 {
 
-    // initialize vertex weights to uniform probability. Double
-    // precision scores are used to avoid underflow for large graphs
-
-    // maybe the nodes will initially be convered? idt need to account for this
-    //cout << "starting" << endl;
     bool converged = false;
     int numNodes = num_nodes(g);
     double equal_prob = 1.0 / numNodes;
-    // try some data locality stuff by putting into 1 array
-    double score_old_array[numNodes];
-    double score_new_array[numNodes];
-    double score_old_divide_array[numNodes];
-    double *score_old = score_old_array;
-    double *score_new = score_new_array;
-    double *score_old_divide = score_old_divide_array;
-    int no_outgoing[numNodes];
-    //cout << "done" << endl;
+    vector<double> score_old_array(numNodes);
+    vector<double> score_new_array(numNodes);
+    vector<double> score_old_divide(numNodes);
+    vector<double>* score_old = &score_old_array;
+    vector<double>* score_new = &score_new_array;
+
+    vector<int> no_outgoing(numNodes);
 
     int list_size = 0;
-    //#pragma omp parallel for
+    #pragma omp parallel for
     for (int i = 0; i < numNodes; i++) {
-        score_new[i] = equal_prob;
+        (*score_new)[i] = equal_prob;
 
         // No outgoing edges
-        //if (!outgoing_size(g, i)) {
-            //int my_index = 0;
-            //#pragma omp atomic capture
-            //{my_index = list_size; list_size++;}
-            //cout << "index" << my_index << endl;
-            //no_outgoing[my_index] = i;
-        //}
+        if (!outgoing_size(g, i)) {
+            int my_index = 0;
+            #pragma omp atomic capture
+            {my_index = list_size; list_size++;}
+            no_outgoing[my_index] = i;
+        } 
     }
-    int chunk_size = 2;
+    int chunk_size = 50;
     while (!converged) {
-        double *temp = score_old;
+        vector<double> *temp = score_old;
         score_old = score_new;
         score_new = temp;
         // try next time to predivide the pagerank vectors by outgoing size
-        //#pragma omp parallel for 
+        #pragma omp parallel for 
         for (int i = 0; i < numNodes; i++) {
             int outgoing = outgoing_size(g, i);
             if (outgoing) {
-                score_old_divide[i] = score_old[i] / outgoing;
+                score_old_divide[i] = (*score_old)[i] / outgoing;
             }
         }
+        double start_val = 0;
+        #pragma imp parallel for reduction(+:start_val)
+        for (int j = 0; j < list_size; j++) {
+          start_val += (*score_old)[no_outgoing[j]];
+        }
+        start_val /= numNodes;
 
         // compute score_new[vi] for all nodes vi:
-        //#pragma omp parallel for schedule(dynamic, chunk_size)
+        #pragma omp parallel for schedule(dynamic, chunk_size)
         for (int i = 0; i < numNodes; i++) {
             const Vertex* begin = incoming_begin(g, i);
             const Vertex* end = incoming_end(g, i);
-            score_new[i] = 0;
+            double mySum = start_val;
             for (const Vertex* v = begin; v != end; v++) {
                 // outgoing can't be 0 here
-                score_new[i] += score_old_divide[*v];
+                mySum += score_old_divide[*v];
             }
-            score_new[i] = (damping * score_new[i]) + (1.0-damping) * equal_prob;
-            for (int j = 0; j < list_size; j++) {
-              score_new[i] += score_old[no_outgoing[j]] / numNodes;
-            }
+
+            mySum = (damping * mySum) + (1.0-damping) * equal_prob;
+            (*score_new)[i] = mySum;
         }
         double global_diff = 0;
-        //#pragma omp parallel for reduction(+:global_diff)
+        #pragma omp parallel for reduction(+:global_diff)
         for (int i = 0; i < numNodes; i++) {
-            global_diff += fabs(score_new[i] - score_old[i]);
+            global_diff += fabs((*score_new)[i] - (*score_old)[i]);
         }
         converged = (global_diff < convergence);
     }
 
-    //#pragma omp parallel for 
+    #pragma omp parallel for 
     for (int i = 0; i < numNodes; i++) {
-        solution[i] = score_new[i];
+        solution[i] = (*score_new)[i];
     }
 }
